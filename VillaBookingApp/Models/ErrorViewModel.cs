a42702e53dfff76175cc91fac38edc51cbd5c505
Fırat Yılmaz <yilmaz8596@gmail.com>
2025-12-23 10:39:17 +0300

    Proje dosyası ekle.

---
+++ VillaBookingApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VillaBookingApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
